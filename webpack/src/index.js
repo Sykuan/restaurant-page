00a57d4e12e36c0b872a5677bca53485a37795ce
Sykuan <william.sun0410@gmail.com>
2024-05-28 18:06:28 +1000

    move files over

---
+++ webpack/src/index.js
@@ -0,0 +1,13 @@
+console.log("testing");
+
+import './style.css';
+
+
+// import funciton
+
+import loadHomepage from './load-Homepage.js';
+
+
+// call function
+
+loadHomepage();
---
+++ webpack/src/load-homepage.js
@@ -0,0 +1,27 @@
+import foodImage from './assets/images/food.jpg';
+
+export default function loadHomepage() {
+    const content = document.getElementById('content');
+    
+    // create headline
+    const headline = document.createElement('h2');
+    headline.textContent = 'Welcome to My Restaurant';
+
+    const text = document.createElement('p');
+    text.textContent = 'This is a really lovely restaurant with food to keep you energised and focussed';
+
+    const imageHolder = document.createElement('div');
+    imageHolder.className = 'imageHolder';
+
+    const image = document.createElement('img');
+    image.src = foodImage;
+    image.alt = 'Image of food being made';
+
+
+    // append elements
+    content.appendChild(headline);
+    content.appendChild(imageHolder);
+    content.appendChild(text);
+
+    imageHolder.appendChild(image);
+}
